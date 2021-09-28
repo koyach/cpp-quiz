45f190776eb7252c9594120becb2acb719acc720
koya <koya1@live.com>
2021-09-28 05:39:44 -0400

    removed unsigned from int

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -10,9 +10,9 @@ int main()
 	vector<int> values = { 4, 3, 5, 7, 2, 4, 6, 4, 66, 2, 3, 32, 53, 62 };
 	int temp;
 
-	for (unsigned int i = 0; i < values.size() - 1; ++i)
+	for (int i = 0; i < values.size() - 1; ++i)
 	{
-		for (unsigned int j = 1; i + j < values.size(); ++j)
+		for (int j = 1; i + j < values.size(); ++j)
 		{
 			if (values[i] > values[i + j])
 			{
@@ -22,7 +22,6 @@ int main()
 			}
 		}
 	}
-
+	
 	for (const auto& value : values) cout << value << " ";
-
 }
